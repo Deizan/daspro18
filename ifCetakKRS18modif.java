1c29f3cea90bfb16757fa0c23245ce95ffd20825
saumsaifulloh <saummm18@gmail.com>
2025-10-01 18:53:14 +0700

    testing

--- ifCetakKRS18modif.java
+++ ifCetakKRS18modif.java
@@ -1,4 +1,4 @@
-//SOAL 1.ifCetakKRS18modif.java
+//ifCetakKRS18modif.java
 //MUHAMMAD SAUM SAIFULLOH /1E/18/SISTEM INFORMASI BISNIS
 
 import java.util.Scanner;
@@ -11,7 +11,7 @@ import java.util.Scanner;
         System.out.print("Apakah UKT sudah lunas? (ya/tidak): ");
         boolean uktLunas = input.nextBoolean();
 
-        String Hasil = uktLunas ? "Pembayaran UKT terverifikasi? Silakan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.";
+        String Hasil = uktLunas ? "Pembayaran UKT terverifikasi Silakan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.";
         System.out.println(Hasil);
 
         input.close();
